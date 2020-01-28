f6dc357666d71fe042229d5f1aeb619d506da380
Aaron Milam <aaron@milam.io>
2020-01-28 06:24:01 -0500

    WIP: react

--- go/react/react.go
+++ go/react/react.go
@@ -1,1 +1,30 @@
 package react
+
+type MyReactor struct {
+}
+
+type MyCell struct {
+	Value int
+}
+
+type MyComputeCell struct {
+
+}
+
+func (reactor MyReactor) CreateInput(value int) MyCell {
+	return MyCell{Value: value}
+}
+
+func (reactor MyReactor) CreateCompute1(cell MyCell, func(int) int) MyComputeCell {
+
+}
+
+CreateCompute2(Cell, Cell, func(int, int) int) ComputeCell
+
+func (cell MyCell) SetValue(value int) {
+	cell.Value = value
+}
+
+func New() MyReactor {
+	return MyReactor{}
+}
